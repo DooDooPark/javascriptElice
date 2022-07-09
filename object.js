887425438f0de8e0b36627d391fc67056f7a55d0
박두현 <doubled4017@gmail.com>
2022-07-09 15:27:19 +0900

    first javascript object

---
+++ object.js
@@ -0,0 +1,11 @@
+
+let user = {
+    name: '우리의두두',
+    age: 30,
+    email: 'test@superstar.com',
+    address: '홍길동 11동',
+    food: ['김치찌게', '제육볶음']
+};
+
+console.log(user);
+console.log(user.name);
